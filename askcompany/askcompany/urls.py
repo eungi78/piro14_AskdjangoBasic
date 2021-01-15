18b033e68d8a5339a6b665939bf46cf75c5de55b
Eungi <schoolday@gmail.com>
2021-01-16 03:08:09 +0900

    오류 수정 중

--- askcompany/askcompany/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.http import HttpResponse
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),  # 어떤 url이 오면 어떤 함수를 호출하겠다.
-    path('shop/', include('shop.urls')),
-    path('blog/', include('blog.urls')),
-]
