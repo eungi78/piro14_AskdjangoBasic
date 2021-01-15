3cef6fbbe74adcd75040cf2c14538719838aa707
Eungi <schoolday@gmail.com>
2021-01-15 13:44:09 +0900

    6강~8강

--- askcompany/shop/converters.py
+++ askcompany/shop/converters.py
@@ -1,7 +1,8 @@
 class FourDigitYearConverter:
     regex = r'\d{4}'
 
-    def to_python(self,value):
+    def to_python(self, value):
         return int(value)
-    def to_url(self,value):
+
+    def to_url(self, value):
         return '%04d' % value
