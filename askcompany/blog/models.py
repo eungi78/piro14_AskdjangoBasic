80f65691eea92a47f00336f331fe0aeb0e86014f
Eungi <schoolday@gmail.com>
2021-01-15 23:55:57 +0900

    15강

--- askcompany/blog/models.py
+++ askcompany/blog/models.py
@@ -15,3 +15,14 @@ class Comment(models.Model):
     # post가 삭제되면 참조값 같이 삭제.
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     message = models.TextField()
+
+
+# Post.objects.all().count() --> 0
+# post1 = Post.objects.create(author_name="author1", title="title1", content="content1")
+# Comment.objects.all().count() --> 0
+# Comment.objects.create(post=post1, message="django")
+# Comment.objects.create(post=post1, message="fucking")
+# Comment.objects.create(post=post1, message="hard")
+# Comment.objects.all().count()
+# post1.comment_set.all()
+# Comment.objects.filter(post=post1)
