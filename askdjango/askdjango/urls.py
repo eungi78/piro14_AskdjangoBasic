833a39454c52d368753a7edae0c1c8d0e70a05b3
Eungi <schoolday@gmail.com>
2021-01-18 15:32:39 +0900

    중급 1강

---
+++ askdjango/askdjango/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.shortcuts import redirect
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
+    path('', lambda req: redirect('/blog/'))  # 최상위 주소로 갈 시 redirect
+]
